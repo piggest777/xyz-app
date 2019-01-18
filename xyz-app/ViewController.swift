c571e986964aadf975c400c94a6510018e31bd15
Denis Rakitin <piggest@PigBook.local>
2019-01-18 18:56:19 +0300

    add outlets

--- xyz-app/ViewController.swift
+++ xyz-app/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var xLbl: UILabel!
+    @IBOutlet weak var yLbl: UILabel!
+    @IBOutlet weak var zLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
