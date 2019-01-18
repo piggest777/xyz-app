1da072263ec357f673b46b3a9737f19f3c3d0b16
Denis Rakitin <piggest@PigBook.local>
2019-01-18 19:21:19 +0300

    add accelerometer manager

--- xyz-app/ViewController.swift
+++ xyz-app/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     @IBOutlet weak var xLbl: UILabel!
     @IBOutlet weak var yLbl: UILabel!
     @IBOutlet weak var zLbl: UILabel!
     
+    var motionManager: CMMotionManager!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLbls)
+    }
+    
+    func updateLbls(data: CMAccelerometerData?,error: Error?)  {
+        guard let accelerometrData = data else {return} 
     }
 
 
